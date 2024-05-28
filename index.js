d309b026963dac2498d1694f3dcc091b88113932
itsmenaihya050723 <angelinesandoval853@gmail.com>
2024-05-28 19:00:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const btnLike1 = document.getElementById("btnLike1")
 const btnLike2 = document.getElementById("btnLike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
+const btnDislike1 = document.getElementById("btnDislike1")
+const btnDislike2 = document.getElementById("btnDislike2")
+const countDislikes1 = document.getElementById("countDislikes1")
+const countDislikes2 = document.getElementById("countDislikes2")
 
 function hitLikes1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
@@ -14,3 +18,15 @@ function hitLikes2() {
   countLikes2.textContent = totalLikes2.toString()
 }
 btnLike2.addEventListener("click",hitLikes2)
+
+function hitDislike1() {
+  let totalDislikes1 = parseInt(countDisikes1.value) + 1
+  countDislikes1.textContent = totalDislikes1.toString()
+}
+btnDislike1.addEventListener("click",hitDislikes1)
+
+function hitDislikes2() {
+  let totalDislikes2 = parseInt(countDisikes2.value) + 1
+  countDislikes2.textContent = totalDlisikes2.toString()
+}
+btnDislike2.addEventListener("click",hitDislikes2)
